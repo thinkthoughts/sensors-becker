8d023bc7f09627e4b329e2009e2fe405cde460ab
dan_hawkley <consensusmechanics@gmail.com>
2026-08-24 11:55:35 -0600

    Update registry.py

--- tools/source_extractors/registry.py
+++ tools/source_extractors/registry.py
@@ -1,12 +1,13 @@
 """Registered source extractors."""
 
-from . import source_00, source_01, source_02, source_03
+from . import source_00, source_01, source_02, source_03, source_04
 
 EXTRACTORS = {
     "SOURCE_00": source_00.extract,
     "SOURCE_01": source_01.extract,
     "SOURCE_02": source_02.extract,
     "SOURCE_03": source_03.extract,
+    "SOURCE_04": source_04.extract,
 }
 
 
